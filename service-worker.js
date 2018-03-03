eeba81de500d1871a3a625701454c28729280d50
seba <sebaczech@gmail.com>
2018-03-04 00:09:18 +0100

    Update project after review: - fix max-width of the restaurant cards for devices with larger screen resolution, - optimize restaurant.html for smaller screens, - add meta viewport tag to restaurant.html, - alternate text is missing for all images, - focus on restaurant.html, - use appropriate ARIA role, - move breadcrumb to nav, - use only 1 H1 on page, the rest is H2 or H3, - register service worker, - implement offline caching in case of network failure.

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+
+  var filesToCache = [
+    '.',
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'index.html',
+    'restaurant.html'
+  ];
+
+  var staticCacheName = 'pages-cache-v1';
+
+  self.addEventListener('install', function(event) {
+    console.log('Service worker installing...');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+    console.log('Service worker activating...');
+  });
+
+  // I'm a new service worker
+
+  self.addEventListener('fetch', function(event) {
+    console.log('Fetching:', event.request.url);
+  });
+
+})();
