1a3981e193e4f93ab7702d8a059de4722baa4073
Sebastian Czech <sebaczech@gmail.com>
2018-03-22 23:47:13 +0100

    Fetch list of restaurants from API, not from file.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -16,19 +16,34 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    // let xhr = new XMLHttpRequest();
+    // xhr.open('GET', DBHelper.DATABASE_URL);
+    // xhr.onload = () => {
+    //   if (xhr.status === 200) { // Got a success response from server!
+    //     const json = JSON.parse(xhr.responseText);
+    //     const restaurants = json.restaurants;
+    //     callback(null, restaurants);
+    //   } else { // Oops!. Got an error from server.
+    //     const error = (`Request failed. Returned status of ${xhr.status}`);
+    //     callback(error, null);
+    //   }
+    // };
+    // xhr.send();
+
+    fetch('http://localhost:1337/restaurants')
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(responseJson) {
+          console.log(responseJson);
+          return responseJson;
+      })
+      .then(function(responseJson) {
+          callback(null, responseJson);
+      })
+      .catch(function(error) {
+          callback(error, null);
+      });
   }
 
   /**
