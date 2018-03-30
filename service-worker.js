fe13a2729027c5388deafa9887e000e35f613d2c
Sebastian Czech <sebaczech@gmail.com>
2018-03-30 20:40:34 +0200

    Fixed code and refactor for reading data from cache, indexedDB or fetching from server.

--- service-worker.js
+++ service-worker.js
@@ -41,125 +41,126 @@
 
   self.addEventListener('fetch', function(event) {
     console.log('Service worker is fetching: ', event.request.url);
-    if (event.request.url.includes('googleapis.com')
-      || event.request.url.includes('gstatic.com')) {
-      fetch(event.request);
-    } else {
       event.respondWith(
         caches.match(event.request).then(function(response) {
           // event respondWith indexedDB onsuccess
           // https://stackoverflow.com/questions/29206836/accessing-indexeddb-in-serviceworker-race-condition
           // https://developers.google.com/web/fundamentals/primers/promises
-          var promiseIndexedDB = new Promise(function(resolve, reject) {
-            const indexedDBName = 'mws-restaurant-db-v1';
-            const storeName = 'mws-restaurant-store-v1';
-            const request = indexedDB.open(indexedDBName, 1 );
+          var promiseIndexedDB = function(url) {
+            console.log('Checking indexedDB for url: ' + url);
+            return new Promise(function(resolve, reject) {
+              if (url.includes("http://localhost:1337/restaurants"))
+              {
+                console.log('Using indexDB for url: ' + url);
 
-            request.onerror = function(event) {
-                reject(request.error);
-            }
+                const indexedDBName = 'mws-restaurant-db-v1';
+                const storeName = 'mws-restaurant-store-v1';
+                const request = indexedDB.open(indexedDBName, 1 );
 
-            request.onupgradeneeded = function(event) {
-                var db = event.target.result;
-                var store = db.createObjectStore(storeName, {keyPath: "id"});
+                request.onerror = function(event) {
+                    reject(request.error);
+                }
 
-                // console.log('IndexedDB is empty - need to fetch data');
-                fetch('http://localhost:1337/restaurants').then(function(response) {
-                     var reply = response.clone();
-                     reply.json().then(function(data) {
-                       console.log('Service worker is adding ' + reply.url + ' to indexed database: ', data);
+                request.onupgradeneeded = function(event) {
+                    var db = event.target.result;
+                    var store = db.createObjectStore(storeName, {keyPath: "id"});
 
-                       // https://gist.github.com/BigstickCarpet/a0d6389a5d0e3a24814b
-                       const indexedDBName = 'mws-restaurant-db-v1';
-                       const storeName = 'mws-restaurant-store-v1';
-                       const request = indexedDB.open(indexedDBName, 1 );
+                    console.log('IndexedDB is empty - need to fetch data');
+                    fetch('http://localhost:1337/restaurants').then(function(response) {
+                         var reply = response.clone();
+                         reply.json().then(function(data) {
+                           console.log('Service worker is adding ' + reply.url + ' to indexed database: ', data);
 
-                       request.onupgradeneeded = function(event) {
-                           var db = event.target.result;
-                           var store = db.createObjectStore(storeName, {keyPath: "id"});
-                       };
+                           // https://gist.github.com/BigstickCarpet/a0d6389a5d0e3a24814b
+                           const indexedDBName = 'mws-restaurant-db-v1';
+                           const storeName = 'mws-restaurant-store-v1';
+                           const request = indexedDB.open(indexedDBName, 1 );
 
-                       request.onsuccess = function(event) {
-                           var db = event.target.result;
-                           var tx = db.transaction(storeName, "readwrite");
-                           var store = tx.objectStore(storeName);
-
-                           data.forEach(function (item) {
-                             store.put(item);
-                           })
-
-                           tx.oncomplete = function() {
-                               db.close();
+                           request.onupgradeneeded = function(event) {
+                               var db = event.target.result;
+                               var store = db.createObjectStore(storeName, {keyPath: "id"});
                            };
-                       }
-                     })
-                });
-            };
 
-            request.onsuccess = function(event) {
-                var db = event.target.result;
-                var tx = db.transaction(storeName, "readwrite");
-                var store = tx.objectStore(storeName);
+                           request.onsuccess = function(event) {
+                               var db = event.target.result;
+                               var tx = db.transaction(storeName, "readwrite");
+                               var store = tx.objectStore(storeName);
 
-                const storeGetAll = store.getAll();
-                var promiseStore = new Promise(function(resolve, reject) {
-                  storeGetAll.onsuccess = function(event) {
-                      resolve(event.target.result);
-                  };
-                  storeGetAll.onerror = function(event) {
-                      reject(storeGetAll.error);
-                  };
-                });
+                               data.forEach(function (item) {
+                                 store.put(item);
+                               })
 
-                tx.onerror = function() {
-                    reject(err);
-                };
-
-                tx.oncomplete = function() {
-                    db.close();
-                    promiseStore.then(function(result) {
-                      // console.log("promiseStore: " + JSON.stringify(result));
-                      resolve(result);
-                    }, function(err) {
-                      // console.log("promiseStore: " + err);
-                      reject(err);
+                               tx.oncomplete = function() {
+                                   db.close();
+                               };
+                           }
+                         })
                     });
                 };
-            }
-          });
+
+                request.onsuccess = function(event) {
+                    var db = event.target.result;
+                    var tx = db.transaction(storeName, "readwrite");
+                    var store = tx.objectStore(storeName);
+
+                    const storeGetAll = store.getAll();
+                    var promiseStore = new Promise(function(resolve, reject) {
+                      storeGetAll.onsuccess = function(event) {
+                          resolve(event.target.result);
+                      };
+                      storeGetAll.onerror = function(event) {
+                          reject(storeGetAll.error);
+                      };
+                    });
+
+                    tx.onerror = function() {
+                        reject(err);
+                    };
+
+                    tx.oncomplete = function() {
+                        db.close();
+                        promiseStore.then(function(result) {
+                          console.log("Content in indexedDB store: " + JSON.stringify(result));
+                          resolve(result);
+                        }, function(err) {
+                          console.log("Error in indexedDB store: " + err);
+                          reject(err);
+                        });
+                    };
+                }
+              } else {
+                console.log('Cannot use indexDB for url: ' + url);
+                reject();
+              }
+            });
+          }
 
           return response
-            || promiseIndexedDB.then(function(result) {
+            || promiseIndexedDB(event.request.url).then(function(result) {
                 console.log('Could not find in cache: ' + event.request.url);
-                if (event.request.url.includes("http://localhost:1337/restaurants")) {
-                  var blob = new Blob([JSON.stringify(result)], {type : 'application/json'});
-                  var response = new Response(blob);
-                  console.log('Returning data from indexedDB: ' + response);
-                  return response;
-                } else {
-                  // console.log('Fetch url: ' + event.request.url);
-                  // fetch(event.request).then(function(reply) {
-                  //     console.log('Returning data from server: ' + reply);
-                  //     return reply;
-                  // });
-                }
+                var blob = new Blob([JSON.stringify(result)], {type : 'application/json'});
+                var response = new Response(blob);
+                console.log('Returning data for ' + event.request.url + ' from indexedDB: ' + response);
+                return response;
               }, function(err) {
-                // fetch(event.request.url).then(function(response) {
-                //     console.log('Returning data from server: ' + response);
-                //     return response;
-                // });
+                console.log('File need to be fetched from url: ' + event.request.url);
+                return fetch(event.request).then(function(response) {
+                    console.log('Returning data ' + event.request.url + ' from server after failed indexedDB: ' + response);
+                    return response;
+                });
               })
-            || fetch(event.request.url).then(function(response) {
-                console.log('Returning data from server: ' + response);
+            || fetch(event.request).then(function(response) {
+                console.log('Returning data for ' + event.request.url + ' from origin: ' + response);
                 return response;
             });
         }, function(err) {
-          // fetch(event.request.url).then(function(response) {
-          //   return response;
-          // });
+          console.log('There is a problem with cache for url: ' + event.request.url);
+          return fetch(event.request).then(function(response) {
+            console.log('Returning data ' + event.request.url + ' from server after failed cache: ' + response);
+            return response;
+          });
         })
       );
-    }
   });
 
 })();
